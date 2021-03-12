06ff871f6041222488016cedbb98869606b9613a
excuseme777 <DatCounter2>
2021-03-12 18:44:42 +0300

    Add new pages and animations with logic for making orders

--- README.md
+++ README.md
@@ -11,8 +11,4 @@ STEPS TO DEVELOP
 
 # TODO:
 
-1. *Scrollbars on services need to be refreshed by update webpage* UPD: check it in deploing
-2. Make buttons as a live
-3. Think about live pages, what can you do
-4. Present Ms. Olga
-5. Add support for mobile devices
+1. Add support for mobile devices
--- scripts/elements.js
+++ scripts/elements.js
@@ -1,5 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    //scrolling enable
+    scrollingEnable();
+    //modal window enable
+    modalWindowEnable();
+    modalWindowRequering();
+    //services enable
     serviceSwithcer();
+    //mask enable for phone
+    $('#phone').inputmask('+7(999)999-99-99', {
+        placeholder: "+7(___)___-__-__",
+        translation: {
+            'r': {
+              pattern: /[^\d+$]/,
+              fallback: '/'
+            },
+        }
+    });
+
     var elements = $(".element");
 
     for (let i = 0; i < elements.length; i++) {
@@ -7,13 +24,115 @@ $(document).ready(function() {
     }
 });
 
+
+
+function modalWindowEnable() {
+    var buttons = $(".button__base");
+    var modalWindow = $(".modal__contact");
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function () {
+            if ($(modalWindow).css('display') === "none") {
+                $(modalWindow).css('display', "block");
+                setTimeout(function () {
+                    $(modalWindow).css('opacity', "1");
+                }, 50);
+            }
+        });
+    }
+    window.onclick = function (event) {
+        if (event.target == modalWindow[0]) {
+            $(modalWindow).css('opacity', "0");
+            setTimeout(function () {
+                $(modalWindow).css('display', "none");
+            }, 500);
+        }
+    }
+}
+
+function modalWindowRequering() {
+    var items = [];
+    items[0] = $(".email input");
+    items[1] = $(".phone input");
+    var button = $(".submit__button");
+
+    button[0].addEventListener('click', function () {
+        if ($.trim(items[0].val()).length === 0 && $.trim(items[1].val()).length === 0) {
+            for (let i = 0; i < items.length; i++) {
+                $(items[i]).attr('required', true);
+            }
+        } else {
+            for (let i = 0; i < items.length; i++) {
+                $(items[i]).removeAttr('required');
+            }
+        }
+
+        if (items[1].val().length > 0 && items[1].val().length != 16)
+        {
+            
+        }
+    });
+}
+
+function scrollingEnable() {
+    var items = $(".options__item");
+
+    for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener('click', function (e) {
+            e.preventDefault();
+
+            var anchor = $(this).attr('data');
+            var item = $(anchor);
+            $('html, body').stop().animate({
+                scrollTop: $(item).offset().top
+            }, 1000);
+        });
+    }
+
+    $(window).scroll(function (e) {
+        $(window).bind('mousewheel DOMMouseScroll MozMousePixelScroll', function (event) {
+            offsetWindow = $(window).scrollTop() + 5;
+            items[0] = $("header");
+            items[1] = $("#services");
+            items[2] = $("#about_us");
+            delta = parseInt(event.originalEvent.wheelDelta || -event.originalEvent.detail);
+            
+            e.preventDefault();
+            if ($(items[0]).offset().top <= offsetWindow && offsetWindow <= items[1].offset().top) {
+                if (delta < 0) {
+                    $('html, body').stop().animate({
+                        scrollTop: $(items[1]).offset().top
+                    }, 1000);
+                }
+            } else if ($(items[1]).offset().top <= offsetWindow && offsetWindow <= items[2].offset().top) {
+                if (delta >= 0) {
+                    $('html, body').stop().animate({
+                        scrollTop: $(items[0]).offset().top
+                    }, 1000);
+                } else {
+                    $('html, body').stop().animate({
+                        scrollTop: $(items[2]).offset().top
+                    }, 1000);
+                }
+            } else if ($(items[2]).offset().top < offsetWindow) {
+                if (delta >= 0) {
+                    $('html, body').stop().animate({
+                        scrollTop: $(items[1]).offset().top
+                    }, 500);
+                }
+            }
+
+        });
+    })
+}
+
 function sortLogicForServices() {
     var services = servicesSort();
     if ($(this).hasClass("left")) {
         $(services[0]).removeClass("item__left");
         $(services[1]).removeClass("item__center");
         $(services[2]).removeClass("item__right");
-        
+
         $(services[0]).addClass("item__right");
         $(services[1]).addClass("item__left");
         $(services[2]).addClass("item__center");
@@ -50,7 +169,7 @@ function servicesSort() {
             sortedServices[2] = services[i];
         }
     }
-    
+
     return sortedServices;
 }
 
